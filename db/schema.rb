be5aeee92b8a9c2f56ef010626f8c01dfda7fdb8
lyh <lyuanhua2008@163.com>
2015-05-23 11:22:52 +0800

    vv

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150429083607) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "emails", force: :cascade do |t|
     t.string   "name"
     t.string   "phone"
